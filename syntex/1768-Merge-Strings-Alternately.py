97d5f5e78d347d588d432b1d8ef0d5a938901a71
gutiantian <william.guxiangtian@gmail.com>
2026-05-16 10:59:36 -0400

    1768. Merge Strings Alternately

---
+++ syntex/1768-Merge-Strings-Alternately.py
@@ -0,0 +1,53 @@
+"""
+1768. Merge Strings Alternately
+You are given two strings word1 and word2.
+Merge the strings by adding letters in alternating order, starting with word1.
+If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Example 1:
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+Example 2:
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b
+word2:    p   q   r   s
+merged: a p b q   r   s
+
+Example 3:
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q
+merged: a p b q c   d
+"""
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        m, n = len(word1), len(word2)
+        res = []
+        index = 0
+
+        while index < min(m, n):
+            res.append(word1[index])
+            res.append(word2[index])
+            index += 1
+
+        while index < m:
+            res.append(word1[index])
+            index += 1
+
+        while index < n:
+            res.append(word2[index])
+            index += 1
+
+        return "".join(res)
