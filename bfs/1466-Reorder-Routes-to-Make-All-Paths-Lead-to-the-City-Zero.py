9a18a190b2c1e63dad8939c90f4482ca0ad9bc59
gutiantian <william.guxiangtian@gmail.com>
2026-06-21 22:16:56 -0400

    1466. Reorder Routes to Make All Paths Lead to the City Zero

---
+++ bfs/1466-Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero.py
@@ -0,0 +1,60 @@
+"""
+1466. Reorder Routes to Make All Paths Lead to the City Zero
+There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is
+only one way to travel between two different cities (this network form a tree).
+Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
+Roads are represented by connections where connections[i] = [ai, bi] represents a road from city ai to city bi.
+This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
+Your task consists of reorienting some roads such that each city can visit the city 0.
+Return the minimum number of edges changed.
+It's guaranteed that each city can reach city 0 after reorder.
+
+
+Example 1:
+Input: n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
+Output: 3
+Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+
+
+Example 2:
+Input: n = 5, connections = [[1,0],[1,2],[3,2],[3,4]]
+Output: 2
+Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+
+
+Example 3:
+Input: n = 3, connections = [[1,0],[2,0]]
+Output: 0
+"""
+
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        map = {}
+
+        for a, b in connections:
+            if a not in map:
+                map[a] = []
+            if b not in map:
+                map[b] = []
+
+            map[a].append((b, -1))
+            map[b].append((a, 1))
+
+        res = 0
+        visited = set()
+        queue = deque()
+        queue.append(0)
+        visited.add(0)
+
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                city = queue.popleft()
+                for (neighbor, direction) in map[city]:
+                    if neighbor not in visited:
+                        if direction == -1:
+                            res += 1
+                        queue.append(neighbor)
+                        visited.add(neighbor)
+
+        return res
